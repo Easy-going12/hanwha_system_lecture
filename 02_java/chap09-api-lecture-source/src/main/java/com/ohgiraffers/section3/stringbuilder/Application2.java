147d9e10837c15725e8db851d0d644524d453d5f
Easy-going12 <qntjf23@naver.com>
2025-08-13 10:13:00 +0900

    문자열 관련 api 작성 완료

---
+++ 02_java/chap09-api-lecture-source/src/main/java/com/ohgiraffers/section3/stringbuilder/Application2.java
@@ -0,0 +1,25 @@
+package com.ohgiraffers.section3.stringbuilder;
+
+public class Application2 {
+    public static void main(String[] args) {
+
+        /* 수업목표. StringBuilder의 자주 사용되는 메소드의 용법 및 원리를 이해할 수 있다. */
+//        StringBuilder sb =  new StringBuilder("javamariaDB");
+        StringBuilder sb =  new StringBuilder("javamariaDB");       // 내부적으로 thread-safe함
+
+        /* 설명. 1. delete() */
+        System.out.println("delete(): " + sb.delete(2,5));
+
+        /* 설명. 2. deleteCharAt() */
+        System.out.println("delete(): " + sb.deleteCharAt(2));
+
+        /* 설명. 3. insert() */
+        System.out.println("insert(): " + sb.insert(1,"vao"));
+        System.out.println("insert(): " + sb.insert(0,"j"));
+        System.out.println("insert(): " + sb.insert((sb.length()),"jdbc"));
+
+        /* 설명. 4. reverse() */
+        System.out.println("reverse(): " + sb.reverse());
+
+    }
+}

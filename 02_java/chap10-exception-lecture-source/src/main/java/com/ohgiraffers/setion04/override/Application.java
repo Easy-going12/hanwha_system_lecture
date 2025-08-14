27a1cfb86d19ce164578feaf4aff3d55d5cf7f95
Easy-going12 <qntjf23@naver.com>
2025-08-14 10:19:15 +0900

    오버라이딩에 관한 예외처리 부분

--- 02_java/chap10-exception-lecture-source/src/main/java/com/ohgiraffers/setion04/override/Application.java
+++ 02_java/chap10-exception-lecture-source/src/main/java/com/ohgiraffers/setion04/override/Application.java
@@ -1,4 +1,16 @@
 package com.ohgiraffers.setion04.override;
 
+import java.io.IOException;
+
 public class Application {
+    public static void main(String[] args) {
+
+        SuperClass superclass = new SubClass();
+        try {
+            superclass.method();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
--- 02_java/chap10-exception-lecture-source/src/main/java/com/ohgiraffers/setion04/override/SuperClass.java
+++ 02_java/chap10-exception-lecture-source/src/main/java/com/ohgiraffers/setion04/override/SuperClass.java
@@ -1,4 +1,9 @@
 package com.ohgiraffers.setion04.override;
 
+import java.io.IOException;
+
 public class SuperClass {
+    public void method() throws IOException {
+
+    }
 }

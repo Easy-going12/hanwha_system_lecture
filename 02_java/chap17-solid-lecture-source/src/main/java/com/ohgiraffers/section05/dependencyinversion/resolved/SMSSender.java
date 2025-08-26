14dece5e3392a2e711297553278c1c107d72263f
Easy-going12 <qntjf23@naver.com>
2025-08-26 09:37:14 +0900

    csv 파일을 활용하는 테스트 케이스 예제 코드 작성

--- 02_java/chap17-solid-lecture-source/src/main/java/com/ohgiraffers/section05/dependencyinversion/resolved/SMSSender.java
+++ 02_java/chap17-solid-lecture-source/src/main/java/com/ohgiraffers/section05/dependencyinversion/resolved/SMSSender.java
@@ -3,6 +3,6 @@ package com.ohgiraffers.section05.dependencyinversion.resolved;
 public class SMSSender implements MessageSender {
     @Override
     public void sendMessage(String message) {
-        System.out.println("SMS 발송: " + message);
+        System.out.println("SMS3 발송: " + message);
     }
 }
--- 03_junit/chap03-parameterizzed-test-lecture-source/src/test/java/com/ohgiraffers/parameterized/section01/params/ParameterizedTests.java
+++ 03_junit/chap03-parameterizzed-test-lecture-source/src/test/java/com/ohgiraffers/parameterized/section01/params/ParameterizedTests.java
@@ -3,12 +3,10 @@ package com.ohgiraffers.parameterized.section01.params;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.NullSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
 import java.time.Month;
+import java.util.stream.Stream;
 
 public class ParameterizedTests {
 
@@ -55,5 +53,61 @@ public class ParameterizedTests {
         int actual = DateValidator.getLastDayOf(month);
 
         Assertions.assertEquals(varifyValue, actual);
+    }
+
+    /* 설명. 4. @CsvSource를 이용한 CSV 리터럴 */
+    @DisplayName("영문자를 대문자로 변경하는지 확인")
+    @ParameterizedTest
+//    @CsvSource({"test:TEST","tEst:TEST","JavaScript:JAVASCRIPT"})
+    @CsvSource(
+            value = {"test:TEST","tEst:TEST","JavaScript:JAVASCRIPT"},
+            delimiter = ':'
+    )
+    void testToUpperCase(String input, String verifyValue){
+        System.out.println("input = " + input);
+        System.out.println("verifyValue = " + verifyValue);
+
+        //when
+        String actual = input.toUpperCase();
+        // 원래는 input.toUpperCase() 부분이 main에서 소문자를 대문자로 변경하는 부분에 해당한다
+
+        //then
+        Assertions.assertEquals(verifyValue, actual);
+    }
+
+    @DisplayName("CSV 파일을 읽은 테스트 데이터를 테스트에 활용되는지 확인")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/parameter-test-data.csv", numLinesToSkip=1)
+    void testToUpperCaseWithCSVFileData(String input, String verifyValue){
+        String actual = input.toUpperCase();
+        Assertions.assertEquals(verifyValue, actual);
+    }
+
+    /* 설명. 5. @MethodSource를 활용한 테스트 인수 활용하기 */
+    /* 설명. 메소드가 반환한 스트림을 호라용해 여러 테스트 케이스를 테스트 해 볼 수 있음 */
+    private static Stream<Arguments> providerStringSource(){
+        return Stream.of(
+                // 앞서 했던 거 보다 확장성이 좋다.
+                Arguments.of("hello world", "HELLO WORLD"),
+                Arguments.of("JavaScript", "JAVASCRIPT"),
+                Arguments.of("tEst", "TEST")
+        );
+    }
+
+    @DisplayName("메소드 소스를 활용한 대문자 변환 테스트")
+    @ParameterizedTest
+    @MethodSource("providerStringSource")
+    void testToUpperCaseWithMethodSource(String input, String verifyValue){
+        String actual = input.toUpperCase();
+        Assertions.assertEquals(verifyValue, actual);
+    }
+
+    /* 설명. 6. ArgumentsProvider를 이용한 메소드 소드 활용 */
+    @DisplayName("두 수를 더한 결과를 정상적으로 반환하는지 테스트")
+    @ParameterizedTest(name = "{index} {0} + {1} = {2} (이)가 맞는지 확인")
+    @ArgumentsSource(SumTwoNumbersArgumentsProvider.class)
+    void testSumTwoNumbers(int num1, int num2, int verifyValue){
+        int actual = Calculator.sumTwoNumbers(num1, num2);
+        Assertions.assertEquals(verifyValue, actual);
     }
 }

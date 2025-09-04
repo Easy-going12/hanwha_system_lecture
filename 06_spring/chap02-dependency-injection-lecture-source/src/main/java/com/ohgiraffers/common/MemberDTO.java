d0a9c1a9e419250ec8281b4d55bd522a51865aa7
Easy-going12 <qntjf23@naver.com>
2025-09-04 15:07:45 +0900

    xml 방식의 DI

--- 06_spring/chap02-dependency-injection-lecture-source/src/main/java/com/ohgiraffers/common/MemberDTO.java
+++ 06_spring/chap02-dependency-injection-lecture-source/src/main/java/com/ohgiraffers/common/MemberDTO.java
@@ -2,8 +2,10 @@ package com.ohgiraffers.common;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor    // xml Setter 주입할 때 필요
 @AllArgsConstructor  // 전체 기본 생성자를 만드는 Annotattion
 public class MemberDTO {
     private int sequence;

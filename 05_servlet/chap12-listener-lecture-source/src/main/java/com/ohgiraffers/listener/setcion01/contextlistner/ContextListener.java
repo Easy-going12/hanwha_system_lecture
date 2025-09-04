c45cb8df9876aa9edfd92d8d6cd9c1da5c127036
Easy-going12 <qntjf23@naver.com>
2025-09-04 09:29:42 +0900

    servlet listener에 관한 예제 코드

---
+++ 05_servlet/chap12-listener-lecture-source/src/main/java/com/ohgiraffers/listener/setcion01/contextlistner/ContextListener.java
@@ -0,0 +1,41 @@
+package com.ohgiraffers.listener.setcion01.contextlistner;
+
+import jakarta.servlet.ServletContextAttributeEvent;
+import jakarta.servlet.ServletContextAttributeListener;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+@WebListener
+// 값을 필수적으로 넣을 필요 없는 추상 클래스로 구성되어졌다.
+public class ContextListener implements ServletContextListener, ServletContextAttributeListener {
+    public ContextListener() {
+        System.out.println("Context listener 인스턴스 생성!");
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("Context init!!");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("Context destroy!!");
+    }
+
+    // 서버에서 로그인 처리할 때 가장 많이 사용된다.
+    @Override
+    public void attributeAdded(ServletContextAttributeEvent event) {
+        System.out.println("attribute add!!" + event.getName());
+    }
+
+    @Override
+    public void attributeRemoved(ServletContextAttributeEvent event) {
+        System.out.println("attribbute remove!!");
+    }
+
+    @Override
+    public void attributeReplaced(ServletContextAttributeEvent event) {
+        System.out.println("attribute replaced!");
+    }
+}
---
+++ 05_servlet/dev/apache-tomcat-10.1.44/webapps/chap12/index.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP - Hello World</title>
+</head>
+<body>
+    <h1>Listener</h1>
+<%--    ul>(lia>a)*2--%>
+    <ul>
+        <lia><a href="context">contest listener test</a></lia>
+        <lia><a href="session">session listener test</a></lia>
+    </ul>
+</body>
+</html>

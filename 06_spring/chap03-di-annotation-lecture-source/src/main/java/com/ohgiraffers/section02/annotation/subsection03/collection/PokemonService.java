1140e708cf4b018c087fa3229c9a4e7e07e70d60
Easy-going12 <qntjf23@naver.com>
2025-09-05 12:17:26 +0900

    다른 외부에서 가져온 annotation 사용하기

--- 06_spring/chap03-di-annotation-lecture-source/src/main/java/com/ohgiraffers/section02/annotation/subsection03/collection/PokemonService.java
+++ 06_spring/chap03-di-annotation-lecture-source/src/main/java/com/ohgiraffers/section02/annotation/subsection03/collection/PokemonService.java
@@ -6,20 +6,38 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service("pokemonServiceCollection")
 public class PokemonService {
 
     /* 설명. 1. 같은 타입의 bean을 List 형태로 주입 */
-    private final List<Pokemon> pokemonList;
+//    private final List<Pokemon> pokemonList;
+//
+//    @Autowired
+//    public PokemonService(List<Pokemon> pokemonList) {
+//        this.pokemonList = pokemonList;
+//    }
+//
+//
+//    public void pokemonAttack() {
+//        pokemonList.forEach(pokemon -> pokemon.attack());
+//    }
 
-    @Autowired
-    public PokemonService(List<Pokemon> pokemonList) {
-        this.pokemonList = pokemonList;
+    /* 설명. 2. 같은 타입의 bean을 Map 형태로 주입 */
+    private final Map<String, Pokemon> pokemonMap;
+
+
+    public PokemonService(Map<String, Pokemon> pokemonMap) {
+        this.pokemonMap = pokemonMap;
     }
 
-
-    public void pokemonAttack() {
-        pokemonList.forEach(pokemon -> pokemon.attack());
+    public void pokemonAttack(){
+        pokemonMap.forEach((key,pokemon) ->{
+            System.out.println("key = " + key);
+            System.out.println("pokemon = " + pokemon);
+            pokemon.attack();
+            System.out.println();
+        });
     }
 }

5d33dfd9680f5f51d7a72de82b655bd479183c2f
Easy-going12 <qntjf23@naver.com>
2025-09-10 12:05:53 +0900

    인터셉트 예제코드

---
+++ 07_springboot/chap06-interceptor-lecture-source/src/main/java/com/ohgiraffers/interceptor/InterceptorTestService.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.interceptor;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InterceptorTestService {
+    public InterceptorTestService() {
+    }
+
+    public void test() {
+        System.out.println("Service bean을 활용한 메소드 호출");
+    }
+}

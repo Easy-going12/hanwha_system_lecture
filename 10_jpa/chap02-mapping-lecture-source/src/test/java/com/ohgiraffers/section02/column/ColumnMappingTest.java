6c470211eec423048e33bda9371792d02072540d
Easy-going12 <qntjf23@naver.com>
2025-09-17 15:42:34 +0900

    jpa 테이블 생성 및 increment 예제 코드

---
+++ 10_jpa/chap02-mapping-lecture-source/src/test/java/com/ohgiraffers/section02/column/ColumnMappingTest.java
@@ -0,0 +1,46 @@
+package com.ohgiraffers.section02.column;
+
+import com.ohgiraffers.section03.primarykey.subsection01.identity.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.*;
+
+public class ColumnMappingTest {
+    private static EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
+
+    @BeforeAll
+    public static void initFactory(){
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
+    }
+
+    @BeforeEach
+    public void initManager(){
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @AfterEach
+    public void closeManager(){
+        entityManager.close();
+    }
+
+    @AfterAll
+    public static void closeFactory(){
+        entityManagerFactory.close();
+    }
+
+    @Test
+    public void 컬럼에서_사용ㅎ하는_속성_테스트(){
+        Member member = new Member();
+        member.setMemberNo(1);
+        member.setMemberId("user01");
+        member.setMemberPwd("pass01");
+        member.setNickname("홍길동");
+        member.setEmail("hong@gmail.com");
+        member.setAddress("서울시 서초구");
+        member.setEnrollDate(new java.util.Date());
+        member.setMemberRole("ROLE_MEMBER");
+        member.setStatus("Y");
+    }
+}

e6bc93177a0cd4f087cf03a82bf86ac218a6e9c5
Easy-going12 <qntjf23@naver.com>
2025-09-17 14:39:55 +0900

    테이블 자동 생성 예제 코드

--- 10_jpa/chap02-mapping-lecture-source/src/test/java/com/ohgiraffers/section01/entity/Member.java
+++ 10_jpa/chap02-mapping-lecture-source/src/test/java/com/ohgiraffers/section01/entity/Member.java
@@ -13,52 +13,140 @@ public class Member {
     @Id
     @Column(name="member_no")
     private int memberNo;
+
     @Column(name="member_id")
     private String memberId;
+
     @Column(name="member_pwd")
     private String memberPwd;
+
     @Column(name="nickname")
     private String nickname;
-    @Column(name="phont")
-    private String phont;
+
+    @Column(name="phone")
+    private String phone;
+
     @Column(name="email")
     private String email;
+
     @Column(name="address")
     private String address;
-    @Column(name="enrollDate")
+
+    @Column(name="enroll_date")
     private java.util.Date enrollDate;
-    @Column(name="memberRole")
+
+    @Column(name="member_role")
     private String memberRole;
+
     @Column(name="status")
     private String status;
 
     public Member() {
     }
 
-    public Member(String address, String email, Date enrollDate, String memberId, int memberNo,
-                  String memberPwd, String memberRole, String nickname, String phont, String status) {
-        this.address = address;
-        this.email = email;
-        this.enrollDate = enrollDate;
-        this.memberId = memberId;
+    public Member(int memberNo, String memberId, String memberPwd, String nickname, String phone, String email, String address, Date enrollDate, String memberRole, String status) {
         this.memberNo = memberNo;
+        this.memberId = memberId;
         this.memberPwd = memberPwd;
-        this.memberRole = memberRole;
         this.nickname = nickname;
-        this.phont = phont;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.enrollDate = enrollDate;
+        this.memberRole = memberRole;
+        this.status = status;
+    }
+
+    public int getMemberNo() {
+        return memberNo;
+    }
+
+    public void setMemberNo(int memberNo) {
+        this.memberNo = memberNo;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getMemberPwd() {
+        return memberPwd;
+    }
+
+    public void setMemberPwd(String memberPwd) {
+        this.memberPwd = memberPwd;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getEnrollDate() {
+        return enrollDate;
+    }
+
+    public void setEnrollDate(Date enrollDate) {
+        this.enrollDate = enrollDate;
+    }
+
+    public String getMemberRole() {
+        return memberRole;
+    }
+
+    public void setMemberRole(String memberRole) {
+        this.memberRole = memberRole;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
         this.status = status;
     }
 
     @Override
     public String toString() {
         return "Member{" +
-                "address='" + address + '\'' +
-                ", memberNo=" + memberNo +
+                "memberNo=" + memberNo +
                 ", memberId='" + memberId + '\'' +
                 ", memberPwd='" + memberPwd + '\'' +
                 ", nickname='" + nickname + '\'' +
-                ", phont='" + phont + '\'' +
+                ", phone='" + phone + '\'' +
                 ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
                 ", enrollDate=" + enrollDate +
                 ", memberRole='" + memberRole + '\'' +
                 ", status='" + status + '\'' +

93673e29d1a0fa6c72ca0b9eddc4e703b7e6d11f
Easy-going12 <qntjf23@naver.com>
2025-09-18 10:22:37 +0900

    Enum 타입을 이용한 값 비교 에제 코드

---
+++ 10_jpa/chap02-mapping-lecture-source/src/test/java/com/ohgiraffers/section05/compositekey/subsection01/embedded/Member.java
@@ -0,0 +1,53 @@
+package com.ohgiraffers.section05.compositekey.subsection01.embedded;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity(name="member_section05_subsection01")
+@Table(name="tbl_member_section05_subsection01")
+public class Member {
+
+    @EmbeddedId
+    private MemberPK memberPK;
+
+    @Column(name="phone")
+    private String phone;
+
+    @Column(name="address")
+    private String address;
+
+    public Member() {
+    }
+
+    public Member(String address, MemberPK memberPK, String phone) {
+        this.address = address;
+        this.memberPK = memberPK;
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public MemberPK getMemberPK() {
+        return memberPK;
+    }
+
+    public void setMemberPK(MemberPK memberPK) {
+        this.memberPK = memberPK;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}

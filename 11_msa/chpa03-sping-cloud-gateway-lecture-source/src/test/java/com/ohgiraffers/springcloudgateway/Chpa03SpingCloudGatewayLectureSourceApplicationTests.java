b9e18bc051b95d0d5e647da86336b9d42fb9c352
Easy-going12 <qntjf23@naver.com>
2025-09-23 12:47:39 +0900

    msa 통신 예제 코드

---
+++ 11_msa/chpa03-sping-cloud-gateway-lecture-source/src/test/java/com/ohgiraffers/springcloudgateway/Chpa03SpingCloudGatewayLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.springcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chpa03SpingCloudGatewayLectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

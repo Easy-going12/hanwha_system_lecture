b92b579d4f36f76ac4d95d9d814871731b7c249f
Easy-going12 <qntjf23@naver.com>
2025-09-24 15:48:38 +0900

    security 코드 예제 코드

--- 11_msa/chap04-01-user-service-lecture-service/src/main/java/com/ohgiraffers/userservice/security/WebSecurity.java
+++ 11_msa/chap04-01-user-service-lecture-service/src/main/java/com/ohgiraffers/userservice/security/WebSecurity.java
@@ -8,8 +8,11 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.Collections;
 
@@ -19,12 +22,14 @@ public class WebSecurity {
 
     private JwtAuthenticationProvider jwtAuthenticationProvider;
     private Environment env;   // JWT Token의 payload에 만료시간 만들다가 추가함
+    private final JwtUtil jwtUtil;
 
     @Autowired
     public WebSecurity(JwtAuthenticationProvider jwtAuthenticationProvider
-            , Environment env) {
+            , Environment env, JwtUtil jwtUtil) {
         this.jwtAuthenticationProvider = jwtAuthenticationProvider;
         this.env = env;
+        this.jwtUtil = jwtUtil;
     }
 
     /* 설명. 우리가 만든 프로바이더 bean으로 등록 */
@@ -34,18 +39,26 @@ public class WebSecurity {
     }
 
     @Bean
-    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    protected SecurityFilterChain configure(HttpSecurity http, JwtUtil jwtUtil) throws Exception {
         http.csrf(csrf -> csrf.disable());
 
         http.authorizeHttpRequests(authz ->
                 authz.requestMatchers("/**").permitAll()
                         .anyRequest().authenticated()
-        );
+        )
+
+        /* 설명. Session 방식이 아닌 JWT Token 방식으로 인증된 화원(Authentication)를 Local Thread로 저장하겠다. */
+        /* 설명. Session 방식이 아닌 JWT Token 방식을 사용하겠다. */
+        .sessionManagement((session ->
+            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)));
+        
+
 
         /* 설명. 매니저를 지닌 필터 등록 */
         http.addFilter(getAuthenticationFilter(authenticationManager()));
 
-        /* 설명. Session 방식이 아닌 JWT Token 방식을 사용하겠다. */
+        /* 설명. 로그인 이후 토큰을 들고 온다면 JwtFilter를 추가해서 검증하도록 함 */
+        http.addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }

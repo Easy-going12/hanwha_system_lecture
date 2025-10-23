0ba25b601b87989979adbe675b8d70ceb3257a6c
Easy-going12 <qntjf23@naver.com>
2025-10-23 15:13:46 +0900

    vue에서 props Drilling 관한 예제 코드

--- 15_vue/08_props/src/App.vue
+++ 15_vue/08_props/src/App.vue
@@ -2,12 +2,16 @@
   <header>
     <PropsParent/>
   </header>
+  <main>
+    <DrillingParent/>
+  </main>
 </template>
 
 <script setup>
   import PropsParent from './components/01_propsBasic/PropsParent.vue';
+  import DrillingParent from './components/02_PropsDrilling/DrillingParent.vue';
 </script>
 
-<style>
+<style scoped>
 
 </style>

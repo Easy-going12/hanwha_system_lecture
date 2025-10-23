e4102f4916ed0d261e23c3b8a773963561543e2b
Easy-going12 <qntjf23@naver.com>
2025-10-23 12:44:42 +0900

    vue에서 prop 활용 예제

---
+++ 15_vue/08_props/src/components/01_propsBasic/PropsParent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <PropsChild :message="message2"/> <!-- message2에 값을 message 변수 값에 넣는다 -->
+    </div>
+</template>
+
+<script setup>
+    import PropsChild from './PropsChild.vue';
+    import {ref} from 'vue';
+
+    const message2 = ref('hello');
+
+</script>
+
+<style scoped>
+
+</style>

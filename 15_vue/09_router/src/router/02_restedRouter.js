34269478dc0a77e6eed9eef30db779aee670c2ff
Easy-going12 <qntjf23@naver.com>
2025-10-24 11:42:59 +0900

    csof에 대한 예제코드

--- 15_vue/09_router/src/router/02_restedRouter.js
+++ 15_vue/09_router/src/router/02_restedRouter.js
@@ -1,10 +1,9 @@
 import {createRouter, createWebHistory} from 'vue-router';
 
 import HomeView from '@/views/01_router/HomeView.vue';
-import NestRouter from '@/views/02_nestedRouter/NestedHome.vue';
+// import NestRouter from '@/views/02_nestedRouter/NestedHome.vue';
 import RootRouter from '@/views/02_nestedRouter/RootRouter.vue';
 import NestedView from '@/views/02_nestedRouter/NestedView.vue';
-import NestedHome from '@/views/02_nestedRouter/NestedHome.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -16,10 +15,18 @@ const router = createRouter({
         {
             path: '/nested',
             component: RootRouter,
+
+            /* children 이후(중첩된 라우팅) 경로는 '/'를 사용하면 안된다.(첫 경로는 '/'를 써야함) */
             children:[
                 {
                 path: 'home',
-                component: NestedHome
+
+                /*
+                    lazy loaded: 코드를 분할(청크)해서 필요한 시점에 가져옴으로써 메모리 낭비를 줄이고
+                                속도 및 성능 향샹을 도모할 수 있다.(코드 스플리팅)
+                */
+                // component: NestedHome
+                component: () => import('@/views/02_nestedRouter/NestedHome.vue')
             },
             {
                 path: 'view',

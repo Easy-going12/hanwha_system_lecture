2744dc1a48f550261fe8454da360d5306a06b3e5
Easy-going12 <qntjf23@naver.com>
2025-11-03 12:24:20 +0900

    네트워크 연결된 상태에서 프론트 엔드 프록시 적용 예제 코드

--- 16_devops/chap01_02_vueproject/vite.config.js
+++ 16_devops/chap01_02_vueproject/vite.config.js
@@ -16,13 +16,23 @@ export default defineConfig({
     },
   },
 
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:8055', // 백엔드 호스트 포트
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ''), // /api 제거 → /plus 로 전달
-  //     }
-  //   }
-  // }
+  server: {
+    host: true,            // 컨테이너 밖에서 접속하려면 필요
+    port: 5173,            // compose에서 8011:5173으로 노출
+    proxy: {
+      '/api': {
+
+        /* 백엔드가 컨테이너화 되기 전 */
+        // target: 'http://localhost:7777'
+
+        /* 백엔드가 컨테이너화 된 후 */
+        // target: 'http://localhost:8055',
+
+        /* docker-compose 이후 도커 네트워크 활용 */
+        target: 'http://springboot-app:7777', // 백엔드 호스트 포트
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // /api 제거 → /plus 로 전달
+      }
+    }
+  }
 })
